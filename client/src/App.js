57e7b9906b8b6d4d1adcce63dbf471aa8589e130
Ujjwal Singh Shekhawat <ujjus2001@gmail.com>
2020-07-04 06:58:26 +0530

    Did some more work on react and added more frontend (learned through Brad Traversy)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => {
   return (
+    <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </switch>
+      </section>
     </Fragment>
+    </Router>
   );
 }
 
